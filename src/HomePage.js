84b666cda8e5a68ac80c34728c01739dddd3b875
yael96334 <yael96334@gmail.com>
2022-05-12 16:18:50 +0300

    homepage

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './Router';
+
+export default function HomePage(){
+return(
+    <p>yaelli</p>
+)
+}
