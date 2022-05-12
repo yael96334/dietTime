a9263352ebfd4a0ceb6fa35ccdf351ebcd392ed8
yael96334 <yael96334@gmail.com>
2022-05-12 14:31:56 +0300

    index

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
+import Router from './Router';
 // import BooksStatus from './BooksStatus';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    
-      <App />
-
+    <BrowserRouter>
+        <App />
+      
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
