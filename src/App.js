4c354273bab4983031733d2a40e3171d2f87ac76
hitmachut <hitmachut@students.local>
2022-05-12 14:25:05 +0300

    add App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import SignUp from './Subscribe';
+import SignUp from './SignIn';
+import Router from './Router';
+import SignIn from './SignIn';
+import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+// import HomePage from './HomePage';
 
 function App() {
+  // const [currentComponenet,setCurrentComponenet]=useState('login')
   return (
     <div className="App">
- <SignUp/>
+     
+        <Router/>
+        {/* < SignIn /> */}
+   
     </div>
   );
 }
