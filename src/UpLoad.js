4e149ad5db42a9ffa4af968b11d63e77a5ea3363
yael96334 <yael96334@gmail.com>
2022-05-18 16:10:07 +0300

    ccc

--- src/UpLoad.js
+++ src/UpLoad.js
@@ -1,39 +1,27 @@
-import React from "react";
-
 import SinglePagePDFViewer from "./SinglePagePDFViewer";
 import samplePDF from "./sample.pdf";
+import React, { useState } from "react";
 
-import "./styles.css";
+export default function PdfImg() {
+    const [pdf, setPdf] = useState(samplePDF);
 
-export default function Upload() {
-  const [pdf, setPdf] = React.useState(samplePDF);
+    return (
+        <>
+            <input
+                type="file"
+                accept="application/pdf"
+                onChange={(e) => {
+                    const file = e.target.files[0];
+                    const reader = new FileReader();
+                    reader.readAsDataURL(file);
+                    reader.onload = () => {
+                        setPdf(reader.result);
+                    }
+                }} />
+            < h4 > Single Page</h4>
+            <SinglePagePDFViewer pdf={pdf} />
+            <hr />
+        </>
+    )
 
-  return (
-    <div className="App">
-      <input
-        type="file"
-        onChange={(e) => {
-          const file = e.target.files[0];
-          const reader = new FileReader();
-          reader.readAsDataURL(file);
-          reader.onload = () => {
-            setPdf(reader.result);
-          };
-        }}
-      />
-
-      <h4>Single Page</h4>
-      <SinglePagePDFViewer pdf={pdf.replace('data:application/pdf;base64,','')} />
-
-      {/* <hr />
-
-     
-      <hr />
-
-      <h4>Base 64 Single Page</h4>
-       <SinglePagePDFViewer pdf={pdf} />
-
-      <hr /> */}
-    </div>
-  );
 }
